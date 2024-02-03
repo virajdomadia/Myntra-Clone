703519a0a105b30d7b850e414398cddb2de0aecf
Viraj <virajdomadia32@gmail.com>
2024-02-03 12:42:07 +0530

    added cart functionality and updated html css and js

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,51 @@
-let bagItem = [];
+let bagItems;
 onLoad();
 
 function onLoad() {
-  let bagItemStr = localStorage.getItem("bagItem");
-  bagItem = bagItemStr ? JSON.parse(bagItemStr) : [];
-  displayItemOnHomePage();
+  let bagItemsStr = localStorage.getItem("bagItems");
+  bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+  displayItemsOnHomePage();
   displayBagIcon();
 }
 
 function addToBag(itemId) {
-  bagItem.push(itemId);
-  localStorage.setItem("bagItem", JSON.stringify(bagItem));
+  bagItems.push(itemId);
+  localStorage.setItem("bagItems", JSON.stringify(bagItems));
   displayBagIcon();
 }
 
 function displayBagIcon() {
   let bagItemCountElement = document.querySelector(".bag-item-count");
-  if (bagItem.length > 0) {
+  if (bagItems.length > 0) {
+    console.log("I am here");
     bagItemCountElement.style.visibility = "visible";
-    bagItemCountElement.innerText = bagItem.length;
+    bagItemCountElement.innerText = bagItems.length;
   } else {
     bagItemCountElement.style.visibility = "hidden";
   }
 }
 
-function displayItemOnHomePage() {
+function displayItemsOnHomePage() {
   let itemsContainerElement = document.querySelector(".items-container");
-  console.log(itemsContainerElement);
-  if (itemsContainerElement === undefined) {
+  if (!itemsContainerElement) {
     return;
   }
-  let innerHtml = " ";
+  let innerHtml = "";
   items.forEach((item) => {
     innerHtml += `
     <div class="item-container">
-        <img
-            src="${item.image}"
-            alt="Product Image"
-            class="item-image"
-        />
-        <div class="rating">${item.rating.stars}⭐ | ${item.rating.count}</div>
-        <div class="company-name">${item.company}</div>
-        <div class="item-name">${item.item_name}</div>
-        <div class="price">
-            <span class="current-price">₹ ${item.current_price}</span>
-            <span class="original-price">₹ ${item.original_price}</span>
-            <span class="discount">(${item.discount_percentage}% OFF)</span>
-        </div>
-        <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+      <img class="item-image" src="${item.image}" alt="item image">
+      <div class="rating">
+          ${item.rating.stars} ⭐ | ${item.rating.count}
+      </div>
+      <div class="company-name">${item.company}</div>
+      <div class="item-name">${item.item_name}</div>
+      <div class="price">
+          <span class="current-price">Rs ${item.current_price}</span>
+          <span class="original-price">Rs ${item.original_price}</span>
+          <span class="discount">(${item.discount_percentage}% OFF)</span>
+      </div>
+      <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
     </div>`;
   });
   itemsContainerElement.innerHTML = innerHtml;
